2de08bb40e66a86403b5ef5fb5682fba1b6a6ed3
Raquel Evangelista <raquel.evangelista2@aluno.ce.gov.br>
2025-06-09 10:20:25 -0300

    crud produtos parte 1

---
+++ crudProdutos/cadastroProdutos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label for="nomeProduto">Nome:</label>
+        <input type="text" name="nomeProduto" id="nomeProduto">
+
+        <label for="preco">Preço:</label>
+        <input type="number" name="preco" id="preco">
+
+        <label for="descricao">Descrição:</label>
+        <input type="textaerea" name="descricao" id="descricao">
+
+        <label for="quantEstoque">Quantidade em estoque:</label>
+        <input type="number" name="quantEstoque" id="quantEstoque">
+        <button type="submit">CADASTRAR</button>
+    </form>
+</body>
+</html>
---
+++ crudProdutos/inserir.php
@@ -0,0 +1,8 @@
+<?php
+include_once "conexao.php";
+
+$nome = $_POST['nomeProduto'];
+$preco = $_POST['preco'];
+$descricao = $_POST['descricao'];
+$quantEstoque = $_POST['quantEstoque'];
+?>
