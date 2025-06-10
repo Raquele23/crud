df8b28d2b0da1dda33d1351e9ae24ea777679d61
Raquel Evangelista <raquel.evangelista2@aluno.ce.gov.br>
2025-06-10 09:10:02 -0300

    update estilo

--- login.php
+++ login.php
@@ -13,12 +13,12 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div id="big-div-login">
-        <div id="tela-parte1">
-            <img src="assets/logoo.jpeg" alt="Logo Ester Acessórios" id="img-logo-form">
-            <h1 id="bem-vindo">Seja bem vindo de volta!</h1>
+    <div class="big-div">
+        <div class="tela-parte1">
+            <img src="assets/logoo.jpeg" alt="Logo Ester Acessórios" class="img-logo-form">
+            <h1 class="bem-vindo">Seja bem vindo de volta!</h1>
         </div>
-        <div id="tela-parte2">
+        <div class="tela-parte2">
             <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24" fill="none" stroke="#ffffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-user-round-icon lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg>
 
             <?php
@@ -41,7 +41,7 @@
             }
             ?>
 
-            <form action="logar.php" method="POST" id="form">
+            <form action="logar.php" method="POST" class="form">
                 <label for="email" class="form-campos">Email:</label>
                 <div class="input-icon">
                     <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="#F582A7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg>
@@ -53,7 +53,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="#F582A7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-lock-keyhole-icon lucide-lock-keyhole"><circle cx="12" cy="16" r="1"/><rect x="3" y="10" width="18" height="12" rx="2"/><path d="M7 10V7a5 5 0 0 1 10 0v3"/></svg>
                     <input type="password" id="senha" name="senha" placeholder="Senha" class="inputs-form">
                 </div>
-                <button type="submit" id="botao-form">Entrar</button>
+                <button type="submit" class="botao-form">Entrar</button>
             </form>
             
             <p id="sem-conta">Não tem uma conta? 
