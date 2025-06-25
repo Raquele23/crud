8511081b514ffc0fab9964212e1fbe511955ecbf
Raquel Evangelista <raquel.evangelista2@aluno.ce.gov.br>
2025-06-25 11:41:12 -0300

    Read funcionário, alterando link no dash e listagemProdutos

---
+++ listagemFun.php
@@ -0,0 +1,97 @@
+<?php
+include_once "conexao.php";
+include_once "protecao.php";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funcionários</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="nav1">
+            <img src="assets/logo2.png" alt="ESTER" class="logo">
+        </div>
+        <div class="nav2">
+            <div class="menu-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-align-justify-icon lucide-align-justify"><path d="M3 12h18"/><path d="M3 18h18"/><path d="M3 6h18"/></svg>
+            </div>
+            <p>DEIXE SUA LUZ BRILHAR!</p>
+            <div class="menu-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bell-icon lucide-bell"><path d="M10.268 21a2 2 0 0 0 3.464 0"/><path d="M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326"/></svg>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <aside class="sidebar">
+            <ul>
+                <li>
+                    <a href="listagemProdutos.php"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-cart-icon lucide-shopping-cart"><circle cx="8" cy="21" r="1"/><circle cx="19" cy="21" r="1"/><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/></svg>
+                    Produtos</a>
+                </li>
+                <li>
+                    <a href=""> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-archive-icon lucide-archive"><rect width="20" height="5" x="2" y="3" rx="1"/><path d="M4 8v11a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8"/><path d="M10 12h4"/></svg>Estoque</a>
+                </li>
+                <li>
+                    <a href="listagemFun.php" class="ativo"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round-search-icon lucide-user-round-search"><circle cx="10" cy="8" r="5"/><path d="M2 21a8 8 0 0 1 10.434-7.62"/><circle cx="18" cy="18" r="3"/><path d="m22 22-1.9-1.9"/></svg> Funcionários</a>
+                </li>
+                <li>
+                    <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-check-big-icon lucide-square-check-big"><path d="M21 10.656V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h12.344"/><path d="m9 11 3 3L22 4"/></svg>Pedidos</a> 
+                </li>
+                <li>
+                    <a href=""> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-message-circle-heart-icon lucide-message-circle-heart"><path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z"/><path d="M15.8 9.2a2.5 2.5 0 0 0-3.5 0l-.3.4-.35-.3a2.42 2.42 0 1 0-3.2 3.6l3.6 3.5 3.6-3.5c1.2-1.2 1.1-2.7.2-3.7"/></svg>Avaliações</a>
+                </li>
+                <li>
+                    <a href="logout.php"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out-icon lucide-log-out"><path d="m16 17 5-5-5-5"/><path d="M21 12H9"/><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/></svg>Logout</a>
+                </li>
+            </ul>
+        </aside>
+
+        <main class="conteudo-principal">
+            <div class="produtos-cadastrar">
+                <h1>Funcionários Cadastrados</h1>
+                <a href="cadastroFun.php" class="botao-cadastrar">Cadastrar Funcionário</a>
+            </div>
+
+            <?php 
+            try {
+                $stmt = $pdo->prepare("SELECT * FROM Funcionario");
+
+                if ($stmt->execute()){
+                while ($funcionario = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+
+                <div class="card-produto">
+                    <div class="info">
+                        <p><strong>Nome: </strong><?= htmlspecialchars($funcionario['nomeFun'])?></p>
+                        <p><strong>Email: </strong> <?= htmlspecialchars($funcionario['email']) ?></p>
+                        <p><strong>Telefone: </strong> <?= htmlspecialchars($funcionario['telefone']) ?></p>
+                        <p><strong>Cargo: </strong> <?= htmlspecialchars($funcionario['cargo']) ?></p>
+
+                        <div class="acoes">
+                            <a class="editar" href="alterarFun.php?act=upd&id=<?= $funcionario['idFuncionario'] ?>">Editar</a>
+                            <a class="excluir" href="deleteFun.php?id=<?= $funcionario['idFuncionario']?>">Excluir</a>
+                        </div>
+                    </div>
+                </div>
+
+                <?php
+                }
+            }
+            
+            
+            }catch (PDOException $erro){
+                echo "Erro: ".$erro->getMessage();
+            }   
+            ?>
+        </main>
+    </div>
+</body>
+</html>
